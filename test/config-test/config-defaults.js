9181385ecda07fcb3d7c4ab2777541f1070906b2
Dan Newman <dan.c.newman@icloud.com>
2021-03-14 22:34:29 -0700

    Unit test config utility

---
+++ test/config-test/config-defaults.js
@@ -0,0 +1,14 @@
+// © 2021 - 2021, ALL RIGHTS RESERVED
+// Ed Estes, Daniel Newman, William J. Steele
+
+'use strict';
+
+module.exports = {
+  foo: {
+    a: 'foo.a config-default',
+    b: 'foo.b config-default',
+    c: 'foo.c config-default',
+    flag: true,
+    int: 314
+  }
+};
